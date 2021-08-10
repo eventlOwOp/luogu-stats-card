ad5ffe8c7c6e8be53f211907ca94272280bdb969
bossbaby2005 <34993620+bossbaby2005@users.noreply.github.com>
2021-08-10 09:44:17 +0800

    Update common.js

--- src/common.js
+++ src/common.js
@@ -10,6 +10,26 @@ const NAMECOLOR = {
 	Cheater: "#ad8b00",
 };
 
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
 class Card {
 	constructor({
 		width = 450,
@@ -80,7 +100,7 @@ class Card {
 			this.hideTitle ? this.paddingY : this.paddingY + this.titleHeight
 		})">
           ${this.body}
-	<text xmlns="http://www.w3.org/2000/svg" x="0" y="295" class="text gray">更新于 ${new Date().toString()}</text>
+	<text xmlns="http://www.w3.org/2000/svg" x="0" y="295" class="text gray">更新于 ${dateFormat("YYYY-mm-dd HH:MM:SS", new Date())}</text>
         </g>
       </svg>`;
 	}
